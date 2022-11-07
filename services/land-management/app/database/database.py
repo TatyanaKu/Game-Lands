00ab47f0884f52439bfa02d3b47e46812c8cb486
Tatyana <tanyaku2801@gmail.com>
2022-11-07 21:40:00 +0300

    Logic of working with PostgreSQL

--- services/land-management/app/database/database.py
+++ services/land-management/app/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://game-lands:game-lands@localhost:27017/game-lands"
+SQLALCHEMY_DATABASE_URL = "postgresql://game-lands:game-lands@localhost:5432/game-lands"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
