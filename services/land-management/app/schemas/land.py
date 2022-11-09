046052538ba8f5805b712833a0eb79e311767015
Tatyana <tanyaku2801@gmail.com>
2022-11-09 17:07:27 +0300

    correct land.py

--- services/land-management/app/schemas/land.py
+++ services/land-management/app/schemas/land.py
@@ -5,7 +5,7 @@ from typing import Optional
 class Land(BaseModel):
     name:str = Field(title='Название земли')
     description: Optional[str] = Field(title='Краткое описание земли')
-    id: int = Field(title='Идентификатор земли')
+    id: int = Field(title='Идентификатор земли', default=None)
 
     class Config:
         orm_mode = True
