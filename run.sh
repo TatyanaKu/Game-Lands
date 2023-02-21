06a53a2c8f088c181be7fd0448f04cd61f33dfd8
Alex <otravnick@gmail.com>
2023-02-22 00:54:23 +0300

    Adapt docker-compose.yaml for run under Linux

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
+echo $PWD
+export POSTGRES_VOLUME_PATH=$PWD/deploy/postgresql/data
+export MONGO_INIT_PATH=$PWD/deploy/mongo/init-mongo.js
+export MONGO_VOLUME_PATH=$PWD/deploy/mongo/data
 docker compose -f deploy/docker-compose.yml up
